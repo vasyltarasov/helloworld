848ac8b5c590624a7f5b8bf7bc2d21fb8a076826
vasyltarasov <tarasov.vasiliy@gmail.com>
2016-10-10 21:05:16 +0300

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
         sayHi();
     }
     static void sayHi(){
-        System.out.println("Hello World! - 2");
+        System.out.println("Hello World! - 3");
     }
 }
