7ff205fcc7ac838d9b74dc7a18521f4457914fcf
Vasil.Tarasov <tarasov.vasiliy@gmail.com>
2016-10-10 21:02:46 +0300

    new

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Vasil.Tarasov on 10.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        sayHi();
+    }
+    static void sayHi(){
+        System.out.println("Hello World! - 2");
+    }
+}
